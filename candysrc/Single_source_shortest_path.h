5e8b7a61d5c4ef190eca3b97f01361010e1bba28
Sriram Srinivasan <42544518+SriramSrinivas@users.noreply.github.com>
2022-11-13 18:30:00 -0600

    SSSP working

--- candysrc/Single_source_shortest_path.h
+++ candysrc/Single_source_shortest_path.h
@@ -18,144 +18,125 @@
 #include <stack>
 /*** All Headers Required From ESSENS **/
 
-void SSPInitialize(int nodes, int src, const A_Network &X, double &startx,
-                   double &endx, vector<RT_Vertex> &SSSP, int p);
 
-using namespace std;
 
 int singleSourceShortestPath(std::string inputGraphFile,
                              std::string remainderGraph,
                              std::string changedEdges, int totalNodes,
                              int threads) {
-  clock_t q, q1, q2, t;
-  double startx, endx, starty, endy;
+    clock_t q, q1, q2, t;
+    double startx, endx, starty, endy;
 
-  /***** Preprocessing to Graph (GUI) ***********/
-  t = clock();
-  q = clock(); // Assume Processed Input
-  // Form node node weight
-  // Nodes are numbered contiguously
-  // Starts from zero
-  // Edges are undirected (both a-->b and b-->a present)
-  // Check if valid input is given
-  //    if ( argc < 7 ) { cout << "INPUT ERROR:: EIGHT inputs required. First:
-  //    filename without tree. Second: Graph Certificate. Third: List of Edges
-  //    to be Updated. Fourth: Upper Bound on Weight Fifth: number of nodes.
-  //    Sixth: Size of batch. Seventh:number of threads\n"; return 0;}
-  //    //Check to see if file opening succeeded
-  //    ifstream the_file ( argv[1] ); if (!the_file.is_open() ) { cout<<"INPUT
-  //    ERROR:: Could not open main //file\n";} ifstream the_file1 ( argv[2] );
-  //    if (!the_file1.is_open() ) { cout<<"INPUT ERROR:: Could not open SCC
-  //    file\n";} ifstream the_file2 ( argv[3] ); if (!the_file2.is_open() ) {
-  //    cout<<"INPUT ERROR:: Could not open update file\n";}
 
-  /* Set the number of Threads and Vertices**/
-  const char *ig = inputGraphFile.c_str();
-  const char *ce = changedEdges.c_str();
-  const char *ipr = remainderGraph.c_str();
-  int maxW = 10;
-  int nodes = totalNodes; // total number of vertices
-  int batch = 2;          // the number of changes processed per point
-  int p = threads;        // total number of threads per core
-  int graphDirectedUndirectedIndicator =
-      0; // Should be 1 for SCC, 0 for not SCC
-         //    ifstream the_file3 ( argv[8] ); if (!the_file3.is_open() ) {
-         //    cout<<"INPUT ERROR:: Could not open SCC file\n";}
+    /***** Preprocessing to Graph (GUI) ***********/
+    t = clock();
+    q = clock();    //Assume Processed Input
 
-  /** Set the number of Threads and Vertices**/
+/* Set the number of Threads and Vertices**/
+    const char* ig = inputGraphFile.c_str();
+    const char* ce = changedEdges.c_str();
+    const char* ipr = remainderGraph.c_str();
 
-  /**   Initializing Memory ***/ /** TBD: Change to Array and Malloc**/
-                                 // startx=omp_get_wtime();
+    int asl=0; //asl denotes the lenght of asynchrony; 0=synchronization at each level
+    // number of levels of asynchrony
+    /** Set the number of Threads and Vertices**/
 
-  // List of Changes
-  // There will be a list for inserts and a list for delete
-  vector<xEdge> allChange;
-  allChange.clear();
-  /*** Initializing Memory ***/
 
-  /*** Read Remainder Edges as Graph ***/
-  startx = omp_get_wtime();
-  A_Network R;
-  // readin_network(&R,  argv[1], -1);
-  readin_graphU(&R, nodes, ig);
-  endx = omp_get_wtime();
-  printf("Total Time for Reading Remainder Network %f \n", endx - startx);
-  /*** Finished Reading CRT Tree **/
-
-  /*** Read the Certificate ***/
-  startx = omp_get_wtime();
-  A_Network X;
-  readin_network(&X, ipr, -1);
-  // readin_graphU(&X, nodes,argv[1]);
-  endx = omp_get_wtime();
-  py::print("Total Time for Reading Certificate Network:", endx - startx);
-  /*** Finished Reading CRT Tree **/
-
-  /*** Read set of Changed Edges ***/
-  startx = omp_get_wtime();
-  readin_changes(ce, &allChange);
-  endx = omp_get_wtime();
-  py::print("Total Time for Reading Update List:", endx - startx);
-  /*** Finished Reading Changed Edges **/
-
-  /** Add Buffer for Extra Nodes ***/
-
-  // Initializing  Rooted Tree
-  RT_Vertex RTV;
-  vector<RT_Vertex> SSSP;
-  vector<SCC_vertex> SCC;
-  if (graphDirectedUndirectedIndicator == 0) {
-    int src = 0; // the source from which the paths are computed
-
+    int maxW = 10;
+    int nodes = totalNodes; // total number of vertices
+    // the number of changes processed per point
+    int p = threads;        // total number of threads per core
+    RT_Vertex RTV;
+    vector<RT_Vertex> SSSP;
     SSSP.clear();
     SSSP.resize(nodes, RTV);
-    SSPInitialize(nodes, src, X, startx, endx, SSSP, p);
-    printf("Total Time for Computing degree and Updating SSSSP %f \n",
-           endx - startx);
+
+    vector<RT_Vertex> SSSP_Org;
+
+
+    //List of Changes
+    //There will be a list for inserts and a list for delete
+    vector<xEdge> allChange;
+    allChange.clear();
+    /*** Initializing Memory ***/
+
+    int src=0; //the source from which the paths are computed
+
+
+    /*** Read the SSSP as a Graph ***/
+    startx=omp_get_wtime();
+    A_Network X;
+    A_Network T;
+    readin_networkM(&X, &T, nodes, ipr, ig);
+    endx=omp_get_wtime();
+    printf("Total Time for Reading Whole Network %f \n", endx-startx);
+    /*** Finished Reading  **/
+
+
+    //Create Rooted Tree
+    startx=omp_get_wtime();
+    create_treeS(&T, &SSSP,src, p);
+    endx=omp_get_wtime();
+    printf("Total Time for Reading SSSP Tree %f \n", endx-startx);
+    SSSP_Org=SSSP;
+    /*** Finished Reading  **/
+
+    /*** Read set of Changed Edges ***/
+    startx=omp_get_wtime();
+    readin_changes(ce, &allChange);
+    endx=omp_get_wtime();
+    printf("Total Time for Reading Update List %f \n", endx-startx);
+    /*** Finished Reading Changed Edges **/
+
+
+    double maxV=(double)maxW*X.size();
+
+
     printf("==Update Starts =======\n");
-    startx = omp_get_wtime();
-    create_treeS(&X, &R, &SSSP, src, p);
-    endx = omp_get_wtime();
-    printf("Total Time for Creating tree %f \n", endx - startx);
 
-    double maxV = (double)maxW * X.size();
+    starty=omp_get_wtime();
 
-    starty = omp_get_wtime();
-    // Update the inserted and delted edges in the tree
-    startx = omp_get_wtime();
-    int te = 0;
-    edge_update(&allChange, &X, &SSSP, &R, &maxV, &te, p);
-    endx = omp_get_wtime();
-    py::print("Total Time for Initial Update:", endx - startx);
+    //Update the inserted and delted edges in the tree
+    startx=omp_get_wtime();
+    edge_update(&allChange, &X, &SSSP, &maxV, p);
+    endx=omp_get_wtime();
+    printf("Total Time for Initial Update  %f\n", endx-startx);
 
-    startx = omp_get_wtime();
-    rest_update(&X, &SSSP, &R, &maxV, &te, p);
-    endx = omp_get_wtime();
-    py::print("Total Time for Complete Update:", endx - startx);
 
-    endy = omp_get_wtime();
-    py::print("Total Time for Initia+Complete Update:", endy - starty, te);
-  }
+    //Update the other vertices based on new inserted edges
+    startx=omp_get_wtime();
+    rest_update(&X, &SSSP, &maxV, asl, p);
+    endx=omp_get_wtime();
+    printf("Total Time for Complete Update  %f\n", endx-startx);
+    //printRT(&SSSP);
 
-  return 0;
-}
+    endy=omp_get_wtime();
 
-void SSPInitialize(int nodes, int src, const A_Network &X, double &startx,
-                   double &endx, vector<RT_Vertex> &SSSP, int p) {
-  startx = omp_get_wtime();
-#pragma omp parallel for num_threads(p)
-  for (int i = 0; i < nodes; i++) {
+    printf("====Total Time %f \n", endy-starty);
 
-    if (i == src) {
-      SSSP[i].Root = -1;
-    } else {
-      SSSP[i].Root = i;
+
+    //Counting
+    int opt_update=0;
+    int total_update=0;
+    int tree_update=0;
+    for(int i=0;i<nodes;i++)
+    {
+        total_update=total_update+SSSP[i].num_update;
+
+        if(SSSP[i].num_update>0)
+        {
+            total_update=total_update+SSSP[i].num_update;}
+
+        if(SSSP_Org[i].Parent!=SSSP[i].Parent)
+        { tree_update=tree_update+1;}
+
+        if(SSSP_Org[i].Dist!=SSSP[i].Dist)
+        { opt_update=opt_update+1;}
     }
-    SSSP[i].Level = 0;
-    SSSP[i].Dist = 0.0;
-    SSSP[i].degree = int(X[i].ListW.size());
-  }
-  endx = omp_get_wtime();
+
+    printf("Optimal Tree Updates %d: Optimal Vertex Updates %d : Total Updates %d \n",tree_update, opt_update, total_update);
+    return 0;
 } // end of main
+
 
 #endif // CANDY_SINGLE_SOURCE_SHORTEST_PATH_H
--- candysrc/readinSSSP_data.h
+++ candysrc/readinSSSP_data.h
@@ -43,154 +43,297 @@
 using namespace std;
 
 #include "SP_structure.hpp"
-// #include "minimum_spanTX.hpp"
+
+
+
+#include "SP_structure.hpp"
+//#include "minimum_spanTX.hpp"
+
 using namespace std;
 
-void readin_SCC(const char *file, vector<SCC_vertex> *SCC) {
+void readin_tree(char *myfile,  vector<RT_Vertex> *SSSP)
+{
+    //File reading parameters
+    FILE *graph_file;
+    char line[128];
+    int node;
+    int parent;
+    int dist;
+    int edge_wt;
 
-  FILE *graph_file;
-  char line[128];
-  SCC_vertex myedge;
+    graph_file=fopen(myfile, "r");
+    while(fgets(line,128,graph_file) != NULL)
+    {
+        //Read line
+        sscanf(line,"%d %d %d %d",&node, &parent, &edge_wt, &dist);
+        SSSP->at(node).Parent=parent;
+        SSSP->at(node).Dist=dist;
+        SSSP->at(node).EDGwt=edge_wt;
 
-  graph_file = fopen(file, "r");
-  while (fgets(line, 128, graph_file) != NULL) {
-    sscanf(line, "%d %d ", &myedge.node, &myedge.sccID);
-    SCC->push_back(myedge);
-  }
-  fclose(graph_file);
+    }//end of while
+    fclose(graph_file);
+
+    return ;
+}//end of function
+
+
+void readin_changes(const char *myfile,  vector<xEdge> *allChange)
+{
+    //File reading parameters
+    FILE *graph_file;
+    char line[128];
+    xEdge myedge;
+    int ins;
+
+    graph_file=fopen(myfile, "r");
+    while(fgets(line,128,graph_file) != NULL)
+    {
+        //Read line
+        sscanf(line,"%d %d %lf %d",&myedge.theEdge.node1,&myedge.theEdge.node2,&myedge.theEdge.edge_wt, &ins);
+        if(ins==1)
+        {myedge.inst=true;}
+        else
+        {myedge.inst=false;}
+
+        // myedge.theEdge.edge_wt=((int)(ceil(myedge.theEdge.edge_wt))%max_val+1);
+        allChange->push_back(myedge);
+
+    }//end of while
+    fclose(graph_file);
+
+    return ;
+}//end of function
+
+void readin_remainder(A_Network *R,  int highV, vector<Edge> *reBs)
+{
+    for(int i=0;i<R->size();i++)
+    {
+        for(int j=0;j<R->at(i).ListW.size();j++)
+        {
+            int_double myval;
+            myval=R->at(i).ListW[j];
+
+            if(i>myval.first) {continue;}
+            if(myval.second> double(highV)){continue;}
+
+            Edge myedge;
+            myedge.node1=i;
+            myedge.node2=myval.first;
+            myedge.edge_wt=myval.second;
+
+
+            reBs->push_back(myedge);
+
+
+        }//end of for
+    }//end of for
+
+
+
+    return ;
+}//end of function*/
+
+
+void readin_remainder(char *myfile,  int highV, vector<Edge> *reBs)
+{
+    //File reading parameters
+    FILE *graph_file;
+    char line[128];
+    Edge myedge;
+    int ins;
+
+    graph_file=fopen(myfile, "r");
+    while(fgets(line,128,graph_file) != NULL)
+    {
+        //Read line
+        sscanf(line,"%d %d %lf",&myedge.node1,&myedge.node2,&myedge.edge_wt);
+
+
+        // if(SSSP->at(myedge.node1).Dist > myedge.
+
+        if(myedge.node1>myedge.node2) {continue;}
+        if(myedge.edge_wt> double(highV)){continue;}
+
+        //printf("%d %d ===\n", myedge.node1, myedge.node2);
+        reBs->push_back(myedge);
+
+
+    }//end of while
+    fclose(graph_file);
+
+
+
+
+
+
+    return ;
+}//end of function*/
+
+
+//Assumes the all nodes present
+//Node starts from 0
+//Total number of vertices=nodes and are consecutively arranged
+//reads only the edges in the edge list, does not reverse them to make undirected
+void readin_graphU(A_Network *X, int nodes, char *myfile)
+{
+
+    //Initialize for X
+    ADJ_Bundle AList; //create current Adj_Bundle
+    X->resize(nodes, AList);
+    int_double colvals;
+
+    //File reading parameters
+    FILE *graph_file;
+    char line[128];
+    int node1, node2;
+    double edge_wt;
+
+
+    graph_file=fopen(myfile, "r");
+    int current_node=0;
+    vector<int_double> ListW;
+    while(fgets(line,128,graph_file) != NULL)
+    {
+        //Read line
+        sscanf(line,"%d %d %lf",&node1,&colvals.first,&colvals.second);
+
+        //Initailize and Add Row
+        if(current_node!=node1)
+        {
+            //if(current_node >-1) //Update network after each row
+            {
+                //Update Rows
+                //X->push_back(AList); //Add row and neighbors to network;
+                X->at(current_node).ListW=ListW;
+                ListW.clear(); //Clear List of Edges;
+
+            } //end of if
+
+            //Set to next node
+            current_node=node1;
+            //AList.Row=current_node;
+        } //end of if
+
+
+        //Add Columns corresponding to the rows
+        //colvals.first=node2;
+        //colvals.second=edge_wt;
+        ListW.push_back(colvals);
+
+
+    }//end of while
+    fclose(graph_file);
+
 }
 
-void readin_changes(const char *myfile, vector<xEdge> *allChange) {
-  // File reading parameters
-  FILE *graph_file;
-  char line[128];
-  xEdge myedge;
-  int ins;
 
-  graph_file = fopen(myfile, "r");
-  while (fgets(line, 128, graph_file) != NULL) {
-    // Read line
-    sscanf(line, "%d %d %lf %d", &myedge.theEdge.node1, &myedge.theEdge.node2,
-           &myedge.theEdge.edge_wt, &ins);
-    if (ins == 1) {
-      myedge.inst = true;
-    } else {
-      myedge.inst = false;
+/**** Reads in two files to the same data structure: No common edge sin the files ***/
+void readin_networkM(A_Network *X, A_Network *T, int nodes,  const char *myfile1,  const char *myfile2)
+{
+
+    //printf("%d==\n",nodes);
+
+    //Initialize for X
+    ADJ_Bundle AList; //create current Adj_Bundle
+    X->resize(nodes, AList);
+    int_double colval;
+
+    for(int i=0;i<nodes;i++)
+    { X->at(i).ListW.clear();
+        X->at(i).Ops.clear();
+        X->at(i).Row=i;
     }
 
-    // myedge.theEdge.edge_wt=((int)(ceil(myedge.theEdge.edge_wt))%max_val+1);
-    allChange->push_back(myedge);
+    //Initialize for T
+    T->resize(nodes, AList);
 
-  } // end of while
-  fclose(graph_file);
-
-  return;
-} // end of function
-
-void readin_remainder(A_Network *R, int highV, vector<Edge> *reBs) {
-  for (int i = 0; i < R->size(); i++) {
-    for (int j = 0; j < R->at(i).ListW.size(); j++) {
-      int_double myval;
-      myval = R->at(i).ListW[j];
-
-      if (i > myval.first) {
-        continue;
-      }
-      if (myval.second > double(highV)) {
-        continue;
-      }
-
-      Edge myedge;
-      myedge.node1 = i;
-      myedge.node2 = myval.first;
-      myedge.edge_wt = myval.second;
-
-      reBs->push_back(myedge);
-
-    } // end of for
-  }   // end of for
-
-  return;
-} // end of function*/
-
-void readin_remainder(const char *myfile, int highV, vector<Edge> *reBs) {
-  // File reading parameters
-  FILE *graph_file;
-  char line[128];
-  Edge myedge;
-  int ins;
-
-  graph_file = fopen(myfile, "r");
-  while (fgets(line, 128, graph_file) != NULL) {
-    // Read line
-    sscanf(line, "%d %d %lf", &myedge.node1, &myedge.node2, &myedge.edge_wt);
-
-    // if(SSSP->at(myedge.node1).Dist > myedge.
-
-    if (myedge.node1 > myedge.node2) {
-      continue;
-    }
-    if (myedge.edge_wt > double(highV)) {
-      continue;
+    for(int i=0;i<nodes;i++)
+    { T->at(i).ListW.clear();
+        T->at(i).Ops.clear();
+        T->at(i).Row=i;
     }
 
-    // printf("%d %d ===\n", myedge.node1, myedge.node2);
-    reBs->push_back(myedge);
 
-  } // end of while
-  fclose(graph_file);
+    //File reading parameters
+    FILE *graph_file;
+    char line[128];
+    int node1, node2;
+    double edge_wt;
 
-  return;
-} // end of function*/
 
-// Assumes the all nodes present
-// Node starts from 0
-// Total number of vertices=nodes and are consecutively arranged
-// reads only the edges in the edge list, does not reverse them to make
-// undirected
-void readin_graphU(A_Network *X, int nodes, const char *myfile) {
+    graph_file=fopen(myfile1, "r");
+    while(fgets(line,128,graph_file) != NULL)
+    {
+        //Read line
+        sscanf(line,"%d %d %lf",&node1,&node2,&edge_wt);
 
-  // Initialize for X
-  ADJ_Bundle AList; // create current Adj_Bundle
-  X->resize(nodes, AList);
-  int_double colvals;
+        // printf("%d %d \n", node1, node2);
 
-  // File reading parameters
-  FILE *graph_file;
-  char line[128];
-  int node1, node2;
-  double edge_wt;
+        if(node1>node2){continue;}
 
-  graph_file = fopen(myfile, "r");
-  int current_node = 0;
-  vector<int_double> ListW;
-  while (fgets(line, 128, graph_file) != NULL) {
-    // Read line
-    sscanf(line, "%d %d %lf", &node1, &colvals.first, &colvals.second);
+        //Get the opposite side
+        X->at(node1).Ops.push_back(X->at(node2).ListW.size());
+        X->at(node2).Ops.push_back(X->at(node1).ListW.size());
 
-    // Initailize and Add Row
-    if (current_node != node1) {
-      // if(current_node >-1) //Update network after each row
-      {
-        // Update Rows
-        // X->push_back(AList); //Add row and neighbors to network;
-        X->at(current_node).ListW = ListW;
-        ListW.clear(); // Clear List of Edges;
+        colval.first=node2;
+        colval.second=edge_wt;
+        X->at(node1).ListW.push_back(colval);
 
-      } // end of if
+        colval.first=node1;
+        X->at(node2).ListW.push_back(colval);
 
-      // Set to next node
-      current_node = node1;
-      // AList.Row=current_node;
-    } // end of if
 
-    // Add Columns corresponding to the rows
-    // colvals.first=node2;
-    // colvals.second=edge_wt;
-    ListW.push_back(colvals);
+        //Get the opposite side
+        T->at(node1).Ops.push_back(T->at(node2).ListW.size());
+        T->at(node2).Ops.push_back(T->at(node1).ListW.size());
 
-  } // end of while
-  fclose(graph_file);
+        colval.first=node2;
+        colval.second=edge_wt;
+        T->at(node1).ListW.push_back(colval);
+
+        colval.first=node1;
+        T->at(node2).ListW.push_back(colval);
+
+
+    }//end of while
+    fclose(graph_file);
+
+
+
+
+
+    graph_file=fopen(myfile2, "r");
+    while(fgets(line,128,graph_file) != NULL)
+    {
+        //Read line
+        sscanf(line,"%d %d %lf",&node1,&node2,&edge_wt);
+
+        if(node1>node2){continue;}
+
+        //Get the opposite side
+        X->at(node1).Ops.push_back(X->at(node2).ListW.size());
+        X->at(node2).Ops.push_back(X->at(node1).ListW.size());
+
+        colval.first=node2;
+        colval.second=edge_wt;
+        X->at(node1).ListW.push_back(colval);
+
+        colval.first=node1;
+        X->at(node2).ListW.push_back(colval);
+
+
+    }//end of while
+    fclose(graph_file);
+
+
+
+
+
+
 }
 
-#endif // CANDY_READINSSSP_DATA_H
+
+
+
+
+#endif //CANDY_READINSSSP_DATA_H
